3af25989ce12b8e6d11ac4444cd866a57378ecb4
Yann <yann@zeteoh.com>
2022-05-08 21:42:32 +0900

    feat: implement hero, translation, fix tailwind
    This implements an empty Hero component.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "de"],
+    defaultLocale: "en-US",
+    locales: ["en-US", "ja-JP"],
   },
 };
---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import { appWithTranslation } from "next-i18next";
+import "tailwindcss/tailwind.css";
+
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default appWithTranslation(MyApp);
