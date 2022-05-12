2f8fc3781350126ebcbd54ae6dd36a2b7085e877
Yann LE GUILLY <yann.leguilly@gmail.com>
2022-05-12 11:58:25 +0900

    feat: implement hero, translation (#2)
    This implements an empty Hero component.
    
    Co-authored-by: Yann <yann@zeteoh.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+const { join } = require("path");
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    join(__dirname, "pages/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "components/**/*.{js,ts,jsx,tsx}"),
   ],
   theme: {
     extend: {},
