53bd68d2a2fb3d0449151b820013899bc7188f04
Yann <yann.leguilly@gmail.com>
2025-01-25 16:55:26 +0900

    feat: add red halo in the hero

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -8,17 +8,15 @@
 </script>
 
 <div class="relative">
-	{#if currentLanguage === 'en'}
+	<!-- Top halo -->
+	<div
+		class="absolute left-1/2 top-[100px] -z-10 h-[100px] w-[100px] -translate-x-1/2 transform-gpu overflow-hidden blur-[96px]"
+		aria-hidden="true"
+	>
 		<div
-			class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl"
-			aria-hidden="true"
-		>
-			<div
-				class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[var(--primary)] to-[var(--destructive)] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-			></div>
-		</div>
-	{/if}
+			class="relative left-[calc(50%)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[hsl(var(--destructive))] to-[hsl(var(--primary))] opacity-50"
+		></div>
+	</div>
 
 	<div
 		class="container mx-auto px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40"
@@ -35,10 +33,12 @@
 					{t.getStarted}
 				</a>
 				<a href="/{currentLanguage}/learn-more" class="text-sm font-semibold leading-6">
-					{t.learnMore} <span aria-hidden="true">→</span>
+					{t.learnMore}
+					<span aria-hidden="true">→</span>
 				</a>
 			</div>
 		</div>
+
 		<div class="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
 			<picture>
 				<source srcset="/images/dashboard_hero.webp" type="image/webp" />
@@ -53,15 +53,13 @@
 		</div>
 	</div>
 
-	{#if currentLanguage === 'en'}
+	<!-- Bottom halo -->
+	<div
+		class="absolute bottom-[300px] left-1/2 -z-10 h-[100px] w-[300px] -translate-x-1/2 transform-gpu overflow-hidden blur-[64px]"
+		aria-hidden="true"
+	>
 		<div
-			class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-			aria-hidden="true"
-		>
-			<div
-				class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[var(--primary)] to-[var(--destructive)] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-			></div>
-		</div>
-	{/if}
+			class="relative left-[calc(50%)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[hsl(var(--primary))] to-[hsl(var(--destructive))] opacity-50"
+		></div>
+	</div>
 </div>
